b765009165f7877f6ac568abe1b6a5d85d8d2612
Web <jdiaz@miuandes.com>
2018-04-11 19:32:21 -0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
 	has_many :project
+	validates :email, presence: true, format:{with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
+	validates :name, presence:true 
+	validates :creditcard , presence:true
+	validates :email, uniqueness: true
+	address :address , presence: true
 end
