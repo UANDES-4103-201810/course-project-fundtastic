cab1a09e9a4efd4c309ef3fdb7718e42ca50ad77
Web <jdiaz@miuandes.com>
2018-04-12 18:10:24 -0300

    Create USer Scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,2 @@
 class User < ApplicationRecord
-	has_many :project
-	validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
-	validates :name, presence: true 
-	validates :creditcard, presence: true
-	validates :email, uniqueness: true
-	validates :address, presence: true
-	validates :password, length: {minimum: 9, maximum: 12}
-
 end
