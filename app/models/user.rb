753032a61ce9c65a4c386402f2b67de82e47393d
asnazal <asnazal@miuandes.cl>
2018-04-13 16:50:34 -0300

    d

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,7 @@ class User < ApplicationRecord
 	has_many :user_promise
 	has_one :wishlist
 	validates :name, presence: true
-	validates :email, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ , message: "de nuevo"}
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Error" }
 	validates :email, uniqueness: true
-	validates :email, presence: true
 	validates :password, length: { in: 8..12 }
-	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
-	validates :password, presence: true
 end
