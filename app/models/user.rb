fae3bd20b88bc2002cb7ff881fe2cd16f366bb15
asnazal <asnazal@miuandes.cl>
2018-04-13 17:44:49 -0300

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 	has_many :user_promise
 	has_one :wishlist
 	validates :name, presence: true
-	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Error" }
-	validates :email, uniqueness: true
-	validates :password, length: { in: 8..12 }
+	#validates :email, uniqueness: true
+	#validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "Error" }
+	#validates :password, length: { in: 8..12 }
 end
