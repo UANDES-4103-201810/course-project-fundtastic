0035743f5ff8db799b35c88d9d0241cd98335c5b
Web <jdiaz@miuandes.com>
2018-04-17 16:14:55 -0300

    search bar basico

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,15 @@ class User < ApplicationRecord
 	has_many :user_promise
 	has_one :wishlist
 	validates :name, presence: true
-	#validates :email, uniqueness: true
-	#validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "Error" }
-	#validates :password, length: { in: 8..12 }
+	validates :email, uniqueness: true
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "Error" }
+	validates :password, length: { in: 8..12 }
+
+	def self.search(search)
+		if search
+			where(["name LIKE ?", "%#{search}%"])
+		else
+				all
+		end
+	end
 end
