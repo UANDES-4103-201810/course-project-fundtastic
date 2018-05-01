21362303f50b1cee2b5d82050c2d8ec98ad0138c
Web <jdiaz@miuandes.com>
2018-05-01 14:40:49 -0300

    login/sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 	has_many :fund
 	has_many :user_promise
 	has_one :wishlist
-	validates :name, presence: true
+	#validates :name, presence: true
 	validates :email, uniqueness: true
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "Error" }
 	validates :password, length: { in: 8..12 }
