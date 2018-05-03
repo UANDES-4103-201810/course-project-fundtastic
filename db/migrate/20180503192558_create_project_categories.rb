b269f4f2a694bed26893f814001336332ec5c1be
Web <jdiaz@miuandes.com>
2018-05-03 16:38:00 -0300

    holi

--- db/migrate/20180503192558_create_project_categories.rb
+++ db/migrate/20180503192558_create_project_categories.rb
@@ -1,6 +1,8 @@
 class CreateProjectCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :project_categories do |t|
+      t.references :project, foreign_key: true
+      t.references :category,foreign_key: true
 
       t.timestamps
     end
