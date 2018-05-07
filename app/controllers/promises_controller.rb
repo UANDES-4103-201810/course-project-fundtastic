6107c86a03990fe0a0c7f3d2a835e78e0742e48a
Web <jdiaz@miuandes.com>
2018-05-07 16:43:33 -0300

    intento promises

--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -69,6 +69,6 @@ class PromisesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def promise_params
-      params.fetch(:promise, {})
+      params.require(:promise).permit( :title, :info, :price , :project_id)
     end
 end
--- app/models/promise.rb
+++ app/models/promise.rb
@@ -1,6 +1,6 @@
 class Promise < ApplicationRecord
 	belongs_to :project
-	has_many :user_promise
+	has_many :user_promises
 	validates :price, numericality: {greater_than_or_equal_to: 0}
 	validates :title, presence: true
 	#validates :date, presence: true
