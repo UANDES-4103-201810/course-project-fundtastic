c07d58999021c78c18613942f946430396d30391
jcdc055 <jdiaz@miuandes.com>
2018-05-24 13:48:13 -0400

    oka

--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -71,6 +71,6 @@ class PromisesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def promise_params
-    params.require(:promise).permit( :title, :info, :price, :project_id)
+    params.require(:promise).permit( :title, :info, :price, :project_id, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180519002329) do
+ActiveRecord::Schema.define(version: 20180524174025) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -64,6 +64,10 @@ ActiveRecord::Schema.define(version: 20180519002329) do
     t.integer "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["project_id"], name: "index_promises_on_project_id"
   end
 
