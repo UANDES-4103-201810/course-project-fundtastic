8d292091bfaf0205781a234013a3d023bafa9250
jcdc055 <jdiaz@miuandes.com>
2018-05-25 11:28:16 -0400

    amount

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525051909) do
+ActiveRecord::Schema.define(version: 20180525152624) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20180525051909) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.integer "amount"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
