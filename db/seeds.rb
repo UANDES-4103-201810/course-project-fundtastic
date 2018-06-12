358012f1e58fb3952d3ac009f60d0721d97c91fd
jcdc055 <jdiaz@miuandes.com>
2018-06-12 19:50:19 -0400

    Entrega 4 lista

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create(name:'admin', lastname:'admin', address:'camino del admin', creditcard:'789' , created_at:'2018-05-26 21:31:21', updated_at: '2018-05-26 21:31:21', email: 'fundtastic2018@gmail.com', avatar_file_name: nil, avatar_content_type: nil, avatar_file_size: nil, avatar_updated_at: nil, admin: true, password:'12345678' )
+User.create(name:'admin', lastname:'admin', address:'camino del admin', creditcard:'789' , created_at:'2018-05-26 21:31:21', updated_at: '2018-05-26 21:31:21', email: 'fundtastic2018@gmail.com', admin: true, password:'12345678',avatar: File.new('app/assets/images/Fun.png', 'r') )
 
 Project.create(title: 'galaxy s', info: 'movil for cominication in long distances', goal: 1000, daystogo: '25', release: DateTime.new(2009,9,14,8), user_id: 1,image: File.new('app/assets/images/phone.jpg', 'r'))
 Project.create(title: 'volvito', info: 'awesome and safe automovil to go everywhere', goal: 1000, daystogo: '25', release: DateTime.new(2011,9,14,8), user_id: 1,image: File.new('app/assets/images/volvito.jpeg', 'r'))
