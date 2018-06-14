5b9567b6b9ad496078ef62a54f3d3be621ceedd7
jcdc055 <jdiaz@miuandes.com>
2018-06-14 17:28:39 -0400

    fin

--- app/models/promise.rb
+++ app/models/promise.rb
@@ -1,7 +1,7 @@
 class Promise < ApplicationRecord
 	belongs_to :project
 	has_many :user_promises,:dependent => :destroy
-	has_attached_file :image ,  styles: { medium: "200x200>", big: "500x500" }
+	has_attached_file :image ,  styles: { medium: "120x120>", big: "500x500" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	validates :price, numericality: {greater_than_or_equal_to: 0}
 	validates :title, presence: true
